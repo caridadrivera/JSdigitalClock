ce740e3caaf700f79d2f55e62f90d55ebd355ac7
caridadrivera <ccrive10@gmail.com>
2020-01-17 16:07:23 -0500

    new Date()

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function showTime() {
-    console.log("here")
+function showTime(){
+
+    //date has methods we can use later to extract what we need for our watch.
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let session = "AM";
+
+    if(hours = 0){
+        hours = 12;
+        
+    }
 }
