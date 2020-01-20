9a4734117797b970bf99cb661b10150f08289d3c
caridadrivera <ccrive10@gmail.com>
2020-01-20 13:03:14 -0500

    logic finished

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ function showTime(){
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     let session = "AM";
 
     if(hours = 0){
-        hours = 12;
-        
+        hours = 12;   
     }
+
+    if(hours > 12) {
+        hours = hours - 12;
+        session = "PM";
+    }
+
+    // if the hours, minutes or seconds are less than 10, I want to add a 0 in front of them.
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
 }
